0f4c39f121f79e5ebf04a925114dba555afd4646
Ateeb Ur Rahmaan <104502014+AteebNoOne@users.noreply.github.com>
2023-07-13 05:03:01 +0500

    400 less width pe dekhna

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import './App.css';
 import Header from './components/Header/Header';
 import News from './components/News/News';
 import RocketGif from './components/RocketGif/RocketGif';
+import FuncButtons from './components/funcButtons/funcButtons';
 
 function App() {
   return (
     <div className='bg'>
-    <div className="App">
-      <Header />
-      <News />
+      <div className="App">
+        <Header />
+        <News />
         <RocketGif />
+        <FuncButtons />
       </div>
     </div>
   );
---
+++ src/components/funcButtons/funcButtons.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import styles from './funcButtons.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSyncAlt, faArrowAltCircleDown, faFileInvoiceDollar, faUserCircle } from '@fortawesome/free-solid-svg-icons';
+
+const FuncButtons = () => {
+    return (
+        <div className={styles['buttons-container']}>
+            <div className={styles.singlebtn}>
+                <button>
+                    <FontAwesomeIcon icon={faSyncAlt} />
+                </button>
+                <span>Recharge</span>
+            </div>
+            <div className={styles.singlebtn}>
+                <button>
+                    <FontAwesomeIcon icon={faArrowAltCircleDown} />
+                </button>
+                <span>Withdraw</span>
+            </div>
+            <div className={styles.singlebtn}>
+                <button>
+                    <FontAwesomeIcon icon={faFileInvoiceDollar} />
+                </button>
+                <span>My Bill</span>
+            </div>
+            <div className={styles.singlebtn}>
+                <button>
+                    <FontAwesomeIcon icon={faUserCircle} />
+                </button>
+                <span>Account</span>
+            </div>
+        </div>
+    );
+};
+
+export default FuncButtons;
