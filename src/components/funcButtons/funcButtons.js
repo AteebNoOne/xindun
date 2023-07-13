c3f2b75ac79cc35012b61d9ced0e739cf4f7296f
UmerGillani36 <syedomershahgillani@gmail.com>
2023-07-13 12:33:38 +0500

    func button ui

--- src/components/funcButtons/funcButtons.js
+++ src/components/funcButtons/funcButtons.js
@@ -1,37 +1,42 @@
 import React from 'react';
 import styles from './funcButtons.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSyncAlt, faArrowAltCircleDown, faFileInvoiceDollar, faUserCircle } from '@fortawesome/free-solid-svg-icons';
+import {
+  faSyncAlt,
+  faArrowAltCircleDown,
+  faFileInvoiceDollar,
+  faUserCircle,
+} from '@fortawesome/free-solid-svg-icons';
 
 const FuncButtons = () => {
-    return (
-        <div className={styles['buttons-container']}>
-            <div className={styles.singlebtn}>
-                <button>
-                    <FontAwesomeIcon icon={faSyncAlt} />
-                </button>
-                <span>Recharge</span>
-            </div>
-            <div className={styles.singlebtn}>
-                <button>
-                    <FontAwesomeIcon icon={faArrowAltCircleDown} />
-                </button>
-                <span>Withdraw</span>
-            </div>
-            <div className={styles.singlebtn}>
-                <button>
-                    <FontAwesomeIcon icon={faFileInvoiceDollar} />
-                </button>
-                <span>My Bill</span>
-            </div>
-            <div className={styles.singlebtn}>
-                <button>
-                    <FontAwesomeIcon icon={faUserCircle} />
-                </button>
-                <span>Account</span>
-            </div>
-        </div>
-    );
+  return (
+    <div className={styles['buttons-container']}>
+      <div className={styles.singlebtn}>
+        <button style={{ backgroundColor: '#6868f1' }}>
+          <FontAwesomeIcon icon={faSyncAlt} />
+        </button>
+        <span>Recharge</span>
+      </div>
+      <div className={styles.singlebtn}>
+        <button style={{ backgroundColor: '#adad0c' }}>
+          <FontAwesomeIcon icon={faArrowAltCircleDown} />
+        </button>
+        <span>Withdraw</span>
+      </div>
+      <div className={styles.singlebtn}>
+        <button style={{ backgroundColor: '#c716c7' }}>
+          <FontAwesomeIcon icon={faFileInvoiceDollar} />
+        </button>
+        <span>My Bill</span>
+      </div>
+      <div className={styles.singlebtn}>
+        <button style={{ backgroundColor: '#10107c' }}>
+          <FontAwesomeIcon icon={faUserCircle} />
+        </button>
+        <span>Account</span>
+      </div>
+    </div>
+  );
 };
 
 export default FuncButtons;
